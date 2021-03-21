1ef0608b67de36e19ae21b6fcd2bc69e948ddbaf
scarletgv <scarletgv@hotmail.com>
2021-03-21 00:48:34 -0300

    Update assets e classe Balança

--- LogiKids/LogiKids/balance_scale.cpp
+++ LogiKids/LogiKids/balance_scale.cpp
@@ -11,9 +11,9 @@ BalanceScale::BalanceScale()
 	button = new GameTexture("assets/scale_weights/sprites/botao", 391, 406, true, false);
 
 	// Texturas balança
-	scale[LEVEL]	  = new GameTexture("assets/scale_weights/balanca_equilibrada.png", 202, 338, false, false);
-	scale[LEAN_LEFT]  = new GameTexture("assets/scale_weights/balanca_esquerda.png", 202, 338, false, false);
-	scale[LEAN_RIGHT] = new GameTexture("assets/scale_weights/balanca_direita.png", 202, 338, false, false);
+	scale_level	  = new GameTexture("assets/scale_weights/balanca_equilibrada.png", 202, 338, false, false);
+	scale_left  = new GameTexture("assets/scale_weights/balanca_esquerda.png", 202, 338, false, false);
+	scale_right = new GameTexture("assets/scale_weights/balanca_direita.png", 202, 338, false, false);
 
 	weighs[0] = new GameTexture("assets/scale_weights/zero.png", 393, 441, false, false);
 	weighs[1] = new GameTexture("assets/scale_weights/um.png", 393, 441, false, false);
@@ -32,6 +32,24 @@ BalanceScale::~BalanceScale()
 {
 }
 
+void BalanceScale::renderScale()
+{
+	switch (getScaleState()) {
+	case LEVEL:
+		scale_level->render();
+		break;
+	case LEAN_LEFT:
+		scale_left->render();
+		break;
+	case LEAN_RIGHT:
+		scale_right->render();
+		break;
+	default:
+		scale_level->render();
+		break;
+	}
+}
+
 void BalanceScale::render()
 {
 	background->render();
@@ -39,7 +57,9 @@ void BalanceScale::render()
 	reset->render();
 	solution_area->render();
 
-	scale[scale_state]->render();
+	//scale[scale_state]->render();
+	renderScale();
+
 	button->render();
 
 	weighs[n_weighs]->render();
@@ -50,10 +70,35 @@ void BalanceScale::render()
 	}
 }
 
+void BalanceScale::weigh()
+{
+	int left_count = 0;
+	int right_count = 0;
+
+	if (n_weighs < 2)
+		n_weighs++;
+
+	for (int i = 0; i < MAX_WEIGHS_ON_PLATE; i++)
+	{
+		if((scale_plates[LEFT_PLATE][i] >= 0) && (scale_plates[LEFT_PLATE][i] != lighter_weight))
+			left_count++;
+		if ((scale_plates[RIGHT_PLATE][i] >= 0) && (scale_plates[RIGHT_PLATE][i] != lighter_weight))
+			right_count++;
+	}
+
+	if (left_count < right_count)
+		setScaleState(LEAN_RIGHT);
+	else if( left_count > right_count)
+		setScaleState(LEAN_LEFT);
+	else 
+		setScaleState(LEVEL);
+}
+
 void BalanceScale::handleEvent(SDL_Event* e)
 {
 	help->handleEvent(e);
-	button->handleEvent(e);
+	if (button->handleEvent(e))
+		weigh();
 
 	if (reset->handleEvent(e))
 		resetLevel();
@@ -64,13 +109,108 @@ void BalanceScale::handleEvent(SDL_Event* e)
 	}
 }
 
+Index BalanceScale::getPlateIndex(GameTexture* w)
+{
+	Index index;
+	SDL_Point weightPos = w->getCurrentPosition();
+	int scale_offset = 34 * getScaleState(); // Offset from the changing heights of a balance scale
+
+	int weightCenterX = weightPos.x + (w->getWidth() / 2),
+		weightCenterY = weightPos.y + (w->getHeight() / 2);
+
+	index.plate = -1;
+	index.n = -1;
+
+	// Check if within boundaries of plates
+	if ((weightCenterX >= LEFT_PLATE_X) && (weightCenterX < LEFT_PLATE_X + PLATE_WIDTH)) {
+		if ((weightCenterY >= LEVEL_PLATE_Y - (scale_offset)) && (weightCenterY < LEVEL_PLATE_Y - (scale_offset) + PLATE_HEIGHT)) {
+			// Weight is on Left Plate
+			index.plate = LEFT_PLATE;
+			index.n = (weightCenterX - LEFT_PLATE_X) / (PLATE_WIDTH / MAX_WEIGHS_ON_PLATE);				
+		}
+	}
+	else if ((weightCenterX >= RIGHT_PLATE_X) && (weightCenterX < RIGHT_PLATE_X + PLATE_WIDTH)) {
+		if ((weightCenterY >= LEVEL_PLATE_Y + (scale_offset)) && (weightCenterY < LEVEL_PLATE_Y + (scale_offset) + PLATE_HEIGHT)) {
+			// Weight is on Right Plate
+			index.plate = RIGHT_PLATE;
+			index.n = (weightCenterX - RIGHT_PLATE_X) / (PLATE_WIDTH / MAX_WEIGHS_ON_PLATE);
+		}
+	}	
+	return index;
+}
+
 void BalanceScale::update()
 {
-	//if (!checkWin())
-	//	updateItems();
-	//else {
-	//	gameWin = true;
-	//}
+	if (!checkWin())
+		updateWeights();
+	else {
+		gameWin = true;
+	}
+}
+
+void BalanceScale::updateWeights() {
+	for (int i = 0; i < N_WEIGHTS; i++)
+	{
+		if (weight[i]->dropped())
+		{
+			index = getPlateIndex(weight[i]);
+			printf("Weight dropped at plate %d, n %d.\n", index.plate, index.n);
+			if (index.plate < 0)
+			{
+				weight[i]->resetPosition();
+			}
+			else {
+				if (!addWeightToScale(weight[i], index, i)) {
+					weight[i]->resetPosition();
+				}
+			}
+		}
+		if (weight[i]->grabbed())
+		{
+			removeWeightFromScale(i);
+		}
+		weight[i]->updatePosFromMouseState();
+	}
+}
+
+void BalanceScale::removeWeightFromScale(int i)
+{
+	for (int n = 0; n < MAX_WEIGHS_ON_PLATE; n++)
+	{
+		if (scale_plates[LEFT_PLATE][n] == i)
+			scale_plates[LEFT_PLATE][n] = -1;
+			
+		if (scale_plates[RIGHT_PLATE][n] == i)
+			scale_plates[RIGHT_PLATE][n] = -1;
+	}	
+}
+
+bool BalanceScale::addWeightToScale(GameTexture* w, Index idx, int weight_number)
+{
+	int scale_offset = 34 * getScaleState(), x_offset;
+	int x = -1, y = -1;
+
+	if (idx.plate == LEFT_PLATE)
+	{
+		x_offset = LEFT_PLATE_X;
+		y = LEVEL_PLATE_Y - scale_offset;
+	}		
+	else
+	{
+		x_offset = RIGHT_PLATE_X;
+		y = LEVEL_PLATE_Y + scale_offset;
+	}		
+
+	for (int i = 0; i < MAX_WEIGHS_ON_PLATE; i++)
+	{
+		if (scale_plates[idx.plate][i] < 0) { // Empty spot
+			x = i * (PLATE_WIDTH / MAX_WEIGHS_ON_PLATE) + x_offset;
+			scale_plates[idx.plate][i] = weight_number;
+			w->setPosition(x, y);
+			return true;
+		}
+	}
+	return false;
 }
 
 int BalanceScale::getScaleState()
@@ -80,12 +220,43 @@ int BalanceScale::getScaleState()
 
 void BalanceScale::setScaleState(int state)
 {
-	if(state >= 0 && state < 3)
-		scale_state = state;
+	scale_state = state;
+	SDL_Point currPos;
+	int y = LEVEL_PLATE_Y;
+	int scale_offset = 34 * getScaleState();
+
+	for (int i = 0; i < MAX_WEIGHS_ON_PLATE; i++)
+	{
+		if (scale_plates[LEFT_PLATE][i] >= 0)
+		{
+			currPos = weight[scale_plates[LEFT_PLATE][i]]->getCurrentPosition();
+			weight[scale_plates[LEFT_PLATE][i]]->setPosition(currPos.x, y-scale_offset);
+
+		}
+		if (scale_plates[RIGHT_PLATE][i] >= 0)
+		{
+			currPos = weight[scale_plates[RIGHT_PLATE][i]]->getCurrentPosition();
+			weight[scale_plates[RIGHT_PLATE][i]]->setPosition(currPos.x, y + scale_offset);
+		}
+	}
 }
 
 void BalanceScale::resetLevel()
 {
+	for (int n = 0; n < MAX_WEIGHS_ON_PLATE; n++)
+	{		
+		scale_plates[LEFT_PLATE][n] = -1;		
+		scale_plates[RIGHT_PLATE][n] = -1;
+	}
+
+	for (int i = 0; i < N_WEIGHTS; i++)
+	{
+		weight[i]->resetPosition();
+	}
+
+	setScaleState(LEVEL);
+	n_weighs = 0;
+	// Randomizar peso mais leve
 }
 
 bool BalanceScale::checkWin()
@@ -97,3 +268,5 @@ bool BalanceScale::gameWon()
 {
 	return gameWin;
 }
+
+
--- LogiKids/LogiKids/balance_scale.h
+++ LogiKids/LogiKids/balance_scale.h
@@ -9,37 +9,68 @@
 #define N_WEIGHTS 8
 #define MAX_WEIGHS 2
 
+#define LEFT_PLATE 0
+#define RIGHT_PLATE 1
+
 // Estados da balança
 #define LEVEL 0
 #define LEAN_RIGHT 1
-#define LEAN_LEFT 2
+#define LEAN_LEFT -1
+
+#define PLATE_WIDTH 136
+#define PLATE_HEIGHT 50
+
+#define LEFT_PLATE_X 206
+#define RIGHT_PLATE_X 460
+
+#define LEVEL_PLATE_Y 320
+#define UP_PLATE_Y 288
+#define DOWN_PLATE_Y 355
+
+#define MAX_WEIGHS_ON_PLATE 4
+
+typedef struct Index {
+	int n; // x
+	int plate; // y
+};
 
 class BalanceScale
 {
 public:
 	BalanceScale();
 	~BalanceScale();
+	void renderScale();
 	void render();
+	void weigh();
 	void handleEvent(SDL_Event* e);
 	void update();
+	void updateWeights();
+	void removeWeightFromScale(int i);
+	bool addWeightToScale(GameTexture* w, Index idx, int weight_number);
 	int getScaleState();
 	void setScaleState(int state);
 	void resetLevel();
 	bool checkWin();
 	bool gameWon();
+	Index getPlateIndex(GameTexture* w);
 
 private:
 	GameTexture* background;
 	GameTexture* help;
 	GameTexture* reset;
-	GameTexture* scale[3]; // A balança tem 3 estados
+	GameTexture* scale_level; // A balança tem 3 estados
+	GameTexture* scale_right;
+	GameTexture* scale_left;
 	GameTexture* weight[N_WEIGHTS];
 	GameTexture* solution_area;
 	GameTexture* weighs[MAX_WEIGHS+1]; // +1 para 0
 	GameTexture* button;
+	Index index; // Auxiliary 
+	int scale_plates[2][MAX_WEIGHS_ON_PLATE] = {{-1, -1, -1, -1},
+												{-1, -1, -1, -1}};
 	int scale_state = LEVEL;
 	bool gameWin = false;
 	int n_weighs = 0; // Pesagem inicial em 0
-
+	int lighter_weight = 0;
 };
 
